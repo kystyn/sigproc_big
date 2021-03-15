2537f9c2145760ba37536cbdd9535de84cac1429
kystyn <konstantindamaskinskiy@gmail.com>
2021-03-15 19:35:23 +0300

    First iteration of code is implemented

--- src/main.py
+++ src/main.py
@@ -1,9 +1,24 @@
-from src.image_process import *
-from src.obj_search import *
+from image_process import *
+from obj_search import *
+import sys
 
 if __name__ == '__main__':
-    img, img_edges, lines = prepare_img('../data/photo_2021-02-11_21-55-55.jpg')
+    if len(sys.argv) != 2:
+        print('Использование: python main.py path/to/image')
+        exit(0)
+
+    try:
+        img, img_edges, lines = prepare_img(sys.argv[1])
+        print("Файл: " + sys.argv[1])
+    except FileNotFoundError:
+        print("Файл " + sys.argv[1] + " не найден")
+        exit(1)
+
     edges = find_edges(lines, img_edges)
     tabletop = find_table(edges, img_edges)
-    chair_pos = find_chair_position(edges, img_edges)
+    chair_pos, estimated_line_list = find_chair_position(edges, img_edges)
+    if make_decision(tabletop, chair_pos, estimated_line_list):
+        print("Стул проходит под стол")
+    else:
+        print("Стул не проходит под стол")
     show_processed_img(img, img_edges, lines)
--- src/obj_search.py
+++ src/obj_search.py
@@ -5,8 +5,9 @@ from enum import Enum
 class ChairPosition(Enum):
     VERTICAL = 1
     HORIZONTAL_ALONG_BACK_ON_FLOOR = 2
-    HORIZONTAL_ALONG_BACK_ORTHO_FLOOR =3
+    HORIZONTAL_ALONG_BACK_ORTHO_FLOOR = 3
     HORIZONTAL_ACROSS = 4
+    STILL_UNKNOWN = 5
 
 
 def dist(dot1, dot2):
@@ -18,6 +19,11 @@ def dist_top_leg(tabletop_corner, tableleg):
     return min(
         dist(tabletop_corner, tableleg[0]),
         dist(tabletop_corner, tableleg[1]))
+
+
+# line == (y0, y1)
+def tg_line(imgw, line):
+    return (line[1] - line[0]) / imgw if line[1] != line[0] else inf
 
 
 def find_table(lines, img_edges):
@@ -81,6 +87,8 @@ def find_table(lines, img_edges):
     # detected front top, back bottom
     # in both cases use it
     out_idx = -1 #len(estimated_tabletop_sorted) - 1
+    if len(estimated_tabletop_sorted) == 0:
+        return None
     in_idx = min(len(estimated_tabletop_sorted[out_idx]) - 1, 1)
     tabletop = \
         max(estimated_tabletop_sorted[out_idx][in_idx], key=lambda x: fabs(x[1][0] - x[0][0]))
@@ -104,20 +112,80 @@ def find_chair_position(edges, img_edges):
     :param img_edges:
     :return: ChairPosition exemplar
     """
+    status, estimated_line_list = detect_horizontal(img_edges.shape[1], img_edges.shape[0], edges)
+    if status != ChairPosition.STILL_UNKNOWN:
+        return status, estimated_line_list
+
+    status, estimated_line_list = detect_vertical(img_edges.shape[1], img_edges.shape[0], edges)
+    if status != ChairPosition.STILL_UNKNOWN:
+        return status, estimated_line_list
+
+    return ChairPosition.HORIZONTAL_ACROSS, []
+
+
+def detect_vertical(imgw, imgh, edges):
+    estimated_chair = []
+    for line in edges.keys():
+        if fabs(line[1] - line[0]) < imgh * 0.1 and max(line[0], line[1]) > 0.75 * imgh:
+            chair = max(edges.get(line), key=lambda x: dist(x[0], x[1]))
+            if 0.25 * imgw < dist(chair[0], chair[1]):
+                estimated_chair.append(chair)
+
+    return ChairPosition.HORIZONTAL_ACROSS, estimated_chair
+
+
+def detect_horizontal(imgw, imgh, edges):
+    # list of potential pairs
+    estimated_line_list = []
+    tg10 = 0.17
+    tg15 = 0.27
+    tg30 = 0.58
+    tg45 = 1
+    tg60 = 1.73
+
+    ordered_keys = [key for key in edges.keys()]
+    for i in range(len(ordered_keys)):
+        line1 = ordered_keys[i]
+        tgline1 = tg_line(imgw, line1)
+        if tg15 < fabs(tgline1) < tg60:
+            for j in range(i + 1, len(ordered_keys)):
+                line2 = ordered_keys[j]
+                tgline2 = tg_line(imgw, line2)
+                if tg30 < fabs(tgline2) < tg45 and tgline1 * tgline2 > 0:
+                    if tg10 < fabs((tgline2 - tgline1) / (1 + tgline1 * tgline2)) < tg30 and\
+                            -0.3 * imgh < line1[0] < 1.3 * imgh and\
+                            -0.3 * imgh < line1[1] < 1.3 * imgh and\
+                            -0.3 * imgh < line2[1] < 1.3 * imgh and\
+                            -0.3 * imgh < line2[1] < 1.3 * imgh:
+                        estimated_line_list.append((line1, line2))
+
     low_horizontal_lines_count = 0
     for l in edges.keys():
         low_horizontal_lines_count +=\
-            (img_edges.shape[0] * 0.75 < l[0] < img_edges.shape[0] and
-             img_edges.shape[0] * 0.75 < l[1] < img_edges.shape[0])
+            (imgh * 0.75 < l[0] < imgh and
+             imgh * 0.75 < l[1] < imgh and
+             l[1] - l[0] < 0.1 * imgw)
 
-    if low_horizontal_lines_count > 3:
-        return ChairPosition.HORIZONTAL_ALONG
+    status = ChairPosition.STILL_UNKNOWN
+
+    if len(estimated_line_list) > 0:
+        if low_horizontal_lines_count >= 3:
+            status = ChairPosition.HORIZONTAL_ALONG_BACK_ON_FLOOR
+        else:
+            status = ChairPosition.HORIZONTAL_ALONG_BACK_ORTHO_FLOOR
+
+    if low_horizontal_lines_count >= 2:
+        status = ChairPosition.HORIZONTAL_ALONG_BACK_ON_FLOOR
+
+    return status, estimated_line_list
 
 
-def make_decision(tabletop, chair_pos):
+def make_decision(tabletop, chair_pos, estimated_line_list):
     if chair_pos == ChairPosition.VERTICAL:
         return False
     if chair_pos == ChairPosition.HORIZONTAL_ACROSS:
         return False
-    if chair_pos == ChairPosition.HORIZONTAL_ALONG:
+    if chair_pos == ChairPosition.HORIZONTAL_ALONG_BACK_ORTHO_FLOOR:
+        return True
+    if chair_pos == ChairPosition.HORIZONTAL_ALONG_BACK_ON_FLOOR:
         return True
